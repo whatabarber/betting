8817d14326ab742fd7e6d612e1a7d1fcccefe1d4
whatabarber <77034235+whatabarber@users.noreply.github.com>
2025-07-30 00:02:43 -0500

    Delete data/Betting_AI_AutoUpdater directory

--- data/Betting_AI_AutoUpdater/update_all.py
+++
@@ -1,7 +0,0 @@
-
-from prizepicks_scanner import update_prizepicks_data
-from bovada_scanner import update_bovada_data
-
-update_prizepicks_data()
-update_bovada_data()
-print("Daily betting data updated.")
